f57ba09373abc9ac10694fc1e6f18478de8902cc
phogbtr <phogbtr@gmail.com>
2021-03-24 23:41:19 -0700

    different attempt

---
+++ public/precache-manifest.51fb7f9164d0065ace4cfe0fa8770cac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "369e6049b857bf1436212a85c0e0b8ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e790ef3ef6cc348f203",
+    "url": "/static/css/main.cae5aeaa.chunk.css"
+  },
+  {
+    "revision": "35143ec42bc1a4e01301",
+    "url": "/static/js/2.e122507f.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.e122507f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e790ef3ef6cc348f203",
+    "url": "/static/js/main.0e6d57f4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
