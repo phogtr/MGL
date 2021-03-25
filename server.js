4d314480e9a3f59d2c255c9e732c1197542be143
phogbtr <phogbtr@gmail.com>
2021-03-24 22:42:52 -0700

    deploy to heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { graphqlHTTP } = require("express-graphql");
 const cors = require("cors");
 const schema = require("./schema");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 //app.use(express.urlencoded({ extended: true }));
@@ -17,6 +18,14 @@ app.use(
     graphiql: true,
   })
 );
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on ${PORT}`));
